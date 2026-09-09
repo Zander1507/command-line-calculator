f2ad771caa94ed34e062decc58f75522256037ec
Zander1507 <zandersteenkamp2005@gmail.com>
2026-09-09 11:28:46 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,36 @@
+print("Command-Line Calculator")
+print("-----------------------")
+
+while True:
+
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == "+":
+        result = num1 + num2
+
+    elif operator == "-":
+        result = num1 - num2
+
+    elif operator == "*":
+        result = num1 * num2
+
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Cannot divide by zero.")
+            continue
+
+        result = num1 / num2
+
+    else:
+        print("Invalid operator.")
+        continue
+
+    print("Result:", result)
+
+    again = input("Calculate again? (y/n): ")
+
+    if again.lower() != "y":
+        print("Goodbye!")
+        break
